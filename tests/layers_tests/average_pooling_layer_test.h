cacb54f562031bf3062110d602b4c9d212f4b356
Alexandru-Valentin Musat <alexandruvalentinmusat@gmail.com>
2017-04-17 22:41:32 +0300

    Fixed RuntimeLibrary between projects

--- tests/layers_tests/average_pooling_layer_test.h
+++ tests/layers_tests/average_pooling_layer_test.h
@@ -20,10 +20,13 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include "../stdafx.h"
-#include <nexuralnet\dnn.h>
+#include <nexuralnet\dnn\layers\computational_layers\average_pooling_layer.h>
 using namespace nexural;
 
 TEST(LAYERS_TESTS, AVERAGE_POOLING_LAYER_TESTS)
 {
+	Tensor inputData, trainingData, targetData;
+
+	
 	EXPECT_EQ(1, 1);
 }
