efe1213dd797896553e7235251026f09818ab23e
Alexandru-Valentin Musat <alexandruvalentinmusat@gmail.com>
2017-05-20 11:28:02 +0300

    Added submodules

--- include/nexuralnet/dnn/layers/computational_layers/leaky_relu_layer.h
+++ include/nexuralnet/dnn/layers/computational_layers/leaky_relu_layer.h
@@ -48,17 +48,6 @@ namespace nexural {
 			{
 				float_n value = inputData[i];
 				_outputData[i] = value < 0 ? (float_n)0.01 * value : value;
-
-				// =============== DEBUG =====================================
-#ifdef _ENABLE_NUMERICALLY_STABLE_DEBUG
-				if (std::isnan(_outputData[i])) {
-					throw std::runtime_error("Detected nan value in leaky relu layer (feedforward) | Value: " + std::to_string(_outputData[i]));
-				}
-				else if (std::isinf(_outputData[i])) {
-					throw std::runtime_error("Detected inf value in leaky relu layer (feedforward) | Value: " + std::to_string(_outputData[i]));
-				}
-#endif
-				// =============== DEBUG END ==================================
 			}
 		}
 
@@ -72,17 +61,6 @@ namespace nexural {
 				float_n error = prevLayerErrors[i];
 				float_n value = _internalInputData[i];
 				_layerErrors[i] = value <= 0 ? (float_n)0.01 * error : error;
-
-				// =============== DEBUG =====================================
-#ifdef _ENABLE_NUMERICALLY_STABLE_DEBUG
-				if (std::isnan(_layerErrors[i])) {
-					throw std::runtime_error("Detected nan value in leaky relu layer (backprop - calculating error) | Input value: " + std::to_string(value) + " Error value: " + std::to_string(error));
-				}
-				else if (std::isinf(_layerErrors[i])) {
-					throw std::runtime_error("Detected inf value in leaky relu layer (backprop - calculating error) | Input value: " + std::to_string(value) + " Error value: " + std::to_string(error));
-				}
-#endif
-				// =============== DEBUG END ==================================
 			}
 		}
 
