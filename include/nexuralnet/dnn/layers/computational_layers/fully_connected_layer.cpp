ebcbe076f2bb90fafa70affd156afc771dc7045f
Alexandru-Valentin Musat <alexandruvalentinmusat@gmail.com>
2017-07-02 04:10:09 +0300

    Changed the copyright notice

--- include/nexuralnet/dnn/layers/computational_layers/fully_connected_layer.cpp
+++ include/nexuralnet/dnn/layers/computational_layers/fully_connected_layer.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016-2017 Alexandru-Valentin Musat (alexandruvalentinmusat@gmail.com)
+/* Copyright (C) 2017 Alexandru-Valentin Musat (contact@nexuralsoftware.com)
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
--- samples/xor_gate_with_relu.h
+++ samples/xor_gate_with_relu.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016-2017 Alexandru-Valentin Musat (alexandruvalentinmusat@gmail.com)
+/* Copyright (C) 2017 Alexandru-Valentin Musat (contact@nexuralsoftware.com)
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
@@ -18,7 +18,6 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 #include "stdafx.h"
 
 using namespace nexural;
